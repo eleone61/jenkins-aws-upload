bbea1f55d7647e058e6abc91005bd284355fdafa
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-05-03 17:21:58 -0400

    Update Jenkins_groovy.groovy

--- Jenkins_groovy.groovy
+++ Jenkins_groovy.groovy
@@ -18,10 +18,34 @@ node {
     stage('Test') {
         echo 'Testing'
     }
-    stage('read yaml') {
-                script{data = readYaml(file: 'Jenkins.yaml')}
-                echo data.deployment.window.toString()
+    stage('Create yaml') {
+                def createYAML(apiVersion, environment, appFamily, appName, version, buildID, deploymentType){
+                        sh """
+                        cat << EOF > Jenkins.yaml 
+                         apiVersion: v1
+                         environment: dev
+                         version: 1.0.0
+                         buildID: xyz
+                         kind: deployment
+                         manifest:
+                          - application.jar
+                          - Jenkins.yaml
+                          - dockerfile
+                          - MD5NUM
+                         deployment:
+                          type: container
+                          target: EKS
+                          style: non-intrusive
+                          window: asap
+                        #Optional Fields
+                          healthcheckURI: ":80/irmod/test.html"
+                        EOF
+                    """
         }
+    }
+    stage('Check Yaml') {
+        cat Jenkins.yaml
+}
 }
 
 
