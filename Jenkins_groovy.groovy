239b2c4536a1099a119ca9453866792388c94a20
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-05-04 14:48:07 -0400

    Update Jenkins_groovy.groovy

--- Jenkins_groovy.groovy
+++ Jenkins_groovy.groovy
@@ -1,11 +1,3 @@
-def apiVersion = 'v1'
-def environment = 'dev'
-def AppFamily = 'TestFam'
-def appName = 'Test Application'
-def version = '1.0.0'
-def buildID = 'xyz'
-def deploymentType = 'container'
-
 def createYAML(apiVersion, environment, appFamily, appName, version, buildID, deploymentType){
                         sh """
                         cat << EOF > Jenkins.yaml 
