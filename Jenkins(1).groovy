8fdfda80a9926b4d95d9d1babe11eebe99e5fd69
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-05-06 13:06:28 -0400

    Update Jenkins(1).groovy

--- Jenkins(1).groovy
+++ Jenkins(1).groovy
@@ -40,6 +40,7 @@ node {
     def datas = readYaml file: 'Jenkins.yml'
     datas.deployment.type = 'container' 
     writeYaml file: 'Jenkins.yml', data: datas, overwrite: true
+    sh 'cat Jenkins.yml'
   }
 }
 
