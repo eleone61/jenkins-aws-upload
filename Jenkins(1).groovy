26a5c3a579d13baa1d9711d50f86587a687be717
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-05-06 11:49:22 -0400

    Update Jenkins(1).groovy

--- Jenkins(1).groovy
+++ Jenkins(1).groovy
@@ -4,10 +4,12 @@ def datas = readYAML file: filename
 
 node {
   stage('Read YAML') {
+    def datas = readYAML file: filename
     return datas
   }
 
   stage('Write Yaml') {
+    def datas = readYAML file: filename
     datas.deployment.type = 'container' 
     writeYAML file: filename, data: datas, overwrite: true
   }
