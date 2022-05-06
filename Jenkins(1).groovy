948e77a42528d63951d899f9e2c634630c570135
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-05-06 14:33:46 -0400

    Update Jenkins(1).groovy

--- Jenkins(1).groovy
+++ Jenkins(1).groovy
@@ -1,3 +1,14 @@
+def apiVersion = 'v1'
+def env = 'dev'
+def version = '1.0.0'
+def buildID = 'xyz'
+def appFamily = 'Test Family'
+def appName = 'Test Application'
+def DEPLOY_TYPE = 'container'
+def DEPLOY_TARGET = 'EKS'
+def DEPLOY_STYLE = 'non-intrusive'
+def DEPLOY_WINDOW = 'asap'
+
 def createYAML(){
     sh """
       cat << EOF > Jenkins.yml 
@@ -15,9 +26,9 @@ def createYAML(){
         - MD5NUM
        deployment:
         type: test
-        target: EKS
-        style: non-intrusive
-        window: asap
+        target: test
+        style: test
+        window: test
       #Optional Fields
         healthcheckURI: ":80/irmod/test.html"
     """
@@ -38,7 +49,16 @@ node {
 
   stage('Write Yaml') {
     def datas = readYaml file: 'Jenkins.yml'
-    datas.deployment.type = 'container' 
+    datas.apiVersion = $(apiVersion)
+    datas.environment = $(env)
+    datas.version = $(version)
+    datas.appFamily = $(appFamily)
+    datas.appName = $(appName)
+    datas.buildID = $(buildID)
+    datas.deployment.type = $(DEPLOY_TYPE)
+    datas.deployment.target = $(DEPLOY_TARGET)
+    datas.deployment.style = $(DEPLOY_STYLE)
+    datas.deployment.window = $(DEPLOY_WINDOW)
     writeYaml file: 'Jenkins.yml', data: datas, overwrite: true
     sh 'cat Jenkins.yml'
   }
