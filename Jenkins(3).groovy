539e24a53b46aa82dadc45e279e84a7b0bf4a436
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-05-12 13:09:08 -0400

    Update Jenkins(3).groovy

--- Jenkins(3).groovy
+++ Jenkins(3).groovy
@@ -27,7 +27,7 @@ node {
         }
         sh """
            cat test1.txt
-           sed -e 's/Reading://g' | sed '1d;$d' > 'test1.txt'
+           sed -e 's/Reading://g' | sed '1d;\$d' > 'test1.txt'
            """
        sh 'cat test1.txt'
 //         sh 'pwd'
