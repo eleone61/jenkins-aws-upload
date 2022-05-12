235c1990cf269c183f194c9305aa58b2935a6cfa
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-05-12 12:27:14 -0400

    Update Jenkins(3).groovy

--- Jenkins(3).groovy
+++ Jenkins(3).groovy
@@ -21,18 +21,18 @@ def Yamldata = [
 
 node {  
     stage('Find DATA') {
-//         sh 'cd testzip'
-//         def folder = unzip zipFile: 'testzip.zip', glob: '', read: true String version = v['test1.txt']                    
-//         echo "finding files: ${folder}"
-// //         echo "${folder[0]}"
         sh 'pwd'
-        def folder = sh script: """
-                                zip -sf test.zip | sed '1d;\$d' | sed -r 's/\\s+//g' > file_list.txt
-                                """
-        sh 'ls && pwd'
-        file = readFile('file_list.txt')
-        def lines = file.readLines()
-        println (lines[0])
+        def folder = unzip zipFile: 'test.zip', glob: '', read: true String version = v['test1.txt']                    
+        echo "finding files: ${folder}"
+        echo "${folder[0]}"
+//         sh 'pwd'
+//         def folder = sh script: """
+//                                 zip -sf test.zip | sed '1d;\$d' | sed -r 's/\\s+//g' > file_list.txt
+//                                 """
+//         sh 'ls && pwd'
+//         file = readFile('file_list.txt')
+//         def lines = file.readLines()
+//         println (lines[0])
 //         temp = []
 //         for (int i = 0; i < lines.size(); i++) {
 //         println lines[i]
@@ -41,7 +41,7 @@ node {
 //         echo 'step 1'
 //      }
         
-        Yamldata.manifest = lines
+//         Yamldata.manifest = lines
         
     }
     
