899c79f74203bbd07ea49e32223efe8ffe2c9152
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-05-25 11:56:26 -0400

    Add files via upload

---
+++ Jenkins(4).groovy
@@ -0,0 +1,64 @@
+def env = '$(env)'
+def buildID = '$(buildID)'
+def Recipients = "examplename1@exampleemail.com, examplename2@exampleemail.com"
+
+def Yamldata = [
+    'apiVersion': 'v1',
+    'environment': env,
+    'version': 1.0,
+    'appFamily': 'FBP',
+    'appName': 'FBP-product-app',
+    'buildID': buildID,
+    'kind': 'deployment',
+    'manifest': '',
+    'deployment': [ 
+        'target': 'EKS',
+        'style': 'non-intrusive',
+        'window': 'asap'
+    ],
+    'email' : ''
+]
+
+
+node {  
+    stage('Find DATA') {
+        if ('test1.txt' || 'test2.txt') {
+            sh 'rm test1.txt'
+            sh 'rm test2.txt'
+            sh 'touch test1.txt' 
+        }
+        tee('test1.txt') {
+            unzip dir: '/var/jenkins_home/workspace/Jenkins Test 5/test', zipFile: 'test.zip', read: true String
+        }
+        sh """
+           cat test1.txt
+           sed -r -e 's/Reading://g' -e '1d;\$d' -e 's/\\s+//g' 'test1.txt' > 'test2.txt'
+           """
+       sh 'cat test2.txt'
+       sh 'touch test.md5'
+
+        file = readFile('test2.txt')
+        def lines = file.readLines()
+        lines.add('test.md5')
+        println (lines[0])
+        
+        
+       def Email = sh returnStdout: true, script: """
+                                                    echo -n ${Recipients}|sed 's/ //g' 
+                                                  """
+        println (Email)
+        def emails = Email.tokenize(',')
+        println (emails)
+        
+        
+        Yamldata.manifest = lines
+        Yamldata.email = emails
+        
+    }
+    
+        
+    stage('Write Yaml') {
+        writeYaml file: 'manifest.yaml', data: Yamldata, overwrite: true
+        sh 'cat manifest.yaml'
+        
+    }
---
+++ Job_DSL.groovy
@@ -0,0 +1,35 @@
+ stage('DSL') {
+        withEnv(["jobName=${params.JobName}", "jobDescription=${params.JobDescription}", "jobTrigger=${params.JobTrigger}", "jobScriptpath=${params.JobScriptPath}"]) {
+            echo jobName
+            echo jobDescription
+            echo jobTrigger
+            echo jobScriptpath
+            jobDsl scriptText: """
+                                pipelineJob('$jobName') { 
+                                    definition {
+                                            cpsScm {
+                                                lightweight(true)
+                                                scm {
+                                                    git{
+                                                        branch('*/main')
+                                                        remote{ url('https://github.com/eleone61/jenkins-aws-upload.git') }
+                                                        }
+                                                     }
+                                                scriptPath('$jobScriptpath')
+                                                    }
+                                                 }
+                                            triggers {
+                                                scm('$jobTrigger')
+                                            }
+                                            logRotator {
+                                                daysToKeep(365)
+                                            }
+                                            description('$jobDescription')
+                                            properties {
+                                                 copyArtifactPermission("Jenkins Test 5")
+                                                 disableConcurrentBuilds()
+                                                 }
+                                          }
+                             """
+        }
+    }
