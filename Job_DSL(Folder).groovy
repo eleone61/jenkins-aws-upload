422184e34ac24a4e0f12204920d767302ce203c1
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-05-25 14:48:42 -0400

    Add files via upload

---
+++ Job_DSL(Folder).groovy
@@ -0,0 +1,39 @@
+node {
+stage('DSL') {
+        withEnv(["jobName=${params.JobName}", "jobDescription=${params.JobDescription}", "jobTrigger=${params.JobTrigger}", "jobScriptpath=${params.JobScriptPath}"]) {
+            echo jobName
+            echo jobDescription
+            echo jobTrigger
+            echo jobScriptpath
+            jobDsl scriptText: """
+                                folder('Test Folder')
+                                pipelineJob('$jobName') { 
+                                    definition {
+                                            cpsScm {
+                                              lightweight(true)
+                                              scm {
+                                                  git{
+                                                      branch('*/main')
+                                                      remote{ url('https://github.com/eleone61/jenkins-aws-upload.git') }
+                                                      }
+                                                    }
+                                              scriptPath('$jobScriptpath')
+                                                  }
+                                                }
+                                    triggers {
+                                        scm('$jobTrigger')
+                                    }
+                                    logRotator {
+                                        daysToKeep(365)
+                                    }
+                                    description('$jobDescription')
+                                    properties {
+                                                copyArtifactPermissionProperty {
+                                                    projectNames('onboarding-test') }
+                                                disableConcurrentBuilds()
+                                                 }
+                                          }
+                                """
+        }
+}
+}
