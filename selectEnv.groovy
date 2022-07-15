d67ee544703cd0fecced6ce61c0e22344ba6ab47
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-07-15 13:11:00 -0400

    Update selectEnv.groovy

--- selectEnv.groovy
+++ selectEnv.groovy
@@ -125,7 +125,7 @@ def updateManifest(pipelineENV) {
 	file['environment'] = pipelineENV["environment"].toLowerCase()
 	
 	def changeRequest = pipelineENV["changeRequest"].toString()
-	file.add('Change Request :' + changeRequest)
+	file.add(changeRequest)
 	
 	if("${env.JOB_NAME}".endsWith(".Test")){
 		file['kind'] = "Artifact Deployment"
