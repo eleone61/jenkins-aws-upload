c790bc02d2211f4acabaa6facc4a7b250d394278
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-07-20 16:54:36 -0400

    Create archiveDeploymentLog.groovy

---
+++ archiveDeploymentLog.groovy
@@ -0,0 +1,18 @@
+def date="72022"
+def name="Elijah"
+def appVersion="1.0"
+def targetEnv="dev"
+def approver="LDHVB"
+def crNumber="1234"
+def home = "Home"
+
+def fileName= "${env.JENKINS_HOME}" + "/logs/" + name + "deployment_log.txt"
+def dashboardFileName = "${env.JENKINS_HOME}" + "/logs/ecm_deployment_log.txt
+
+
+sh """
+      set +x
+      echo "DeployDate: $date \tAppName: $name \tAppVersion: $appVersion \tDeployedto: $targetEnv \tApprovedBy: ${approver} \tKISAM CR: $crNumber" >> $fileName
+      cp $fileName test1.txt
+      echo \$(date "+%a %d %b %Y %T %p") "\t${name} \t${appVersion} \t${targetEnv} \t${approver} \t${crNumber} \t${env.BUILD_TAG}" >> $dashboardFileName
+   """
