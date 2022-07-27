ae7fe53cf9dca67ddc3ec2d2173efad1197efe4c
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-07-27 17:33:14 -0400

    Update Console_check.groovy

--- Console_check.groovy
+++ Console_check.groovy
@@ -1,5 +1,5 @@
 node {
-  def consoleCheck = sh returnStdout: false, script:  """  
+  def consoleCheck = sh script:  """  
                                                       if ( [ -s console.log ] )
                                                       then
                                                           echo 'true' 
