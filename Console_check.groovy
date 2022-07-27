4087f8f58375f35d39516d9e7a08af00a95e72f9
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-07-27 16:42:59 -0400

    Update Console_check.groovy

--- Console_check.groovy
+++ Console_check.groovy
@@ -2,14 +2,11 @@ node {
 def consoleCheck = sh returnStdout: true, script:  """  
                                                       if ( [ -s console.log ] )
                                                       then
-                                                          echo "File exists"
                                                           true
                                                       elif [ -f console.log ]
                                                       then
-                                                        echo "File exists but is Null"
                                                         false
                                                     else 
-                                                        echo "File does not exist"
                                                         false
                                                     fi
                                                   """
