73f47b311dac7ed6e1ee8a5c8575abdd88067cdd
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-07-27 17:20:52 -0400

    Update Console_check.groovy

--- Console_check.groovy
+++ Console_check.groovy
@@ -17,7 +17,7 @@ def consoleCheck = sh returnStdout: true, script:  """
 
   stage('Console Check') {
     println(consoleCheck)
-    if (consoleCheck == '') 
+    if (consoleCheck == "false") 
     {
       echo "Console Log exists"
     } 
