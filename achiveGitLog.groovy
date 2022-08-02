659053f29e79849f3844b1625f9d9b40906df587
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-08-02 15:40:12 -0400

    Update achiveGitLog.groovy

--- achiveGitLog.groovy
+++ achiveGitLog.groovy
@@ -1,11 +1,10 @@
 node {
   stage('Archive Git Log') {
-//     git changelog: true,
-//       url: "https://github.com/eleone61/jenkins-aws-upload.git"
-    sh """
-          ls -al
-          git log 'git@github.com:eleone61/jenkins-aws-upload.git' >> git.log
-       """
+       git "https://github.com/eleone61/jenkins-aws-upload.git"
+//     sh """
+//           ls -al
+//           git log 'git@github.com:eleone61/jenkins-aws-upload.git' >> git.log
+//        """
     
     archiveArtifacts artifacts: "git.log", fingerprint: true
   }
