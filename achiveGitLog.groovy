215c117aacda89180bf79f0e3dceaf132a306df3
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-08-04 15:38:36 -0400

    Update achiveGitLog.groovy

--- achiveGitLog.groovy
+++ achiveGitLog.groovy
@@ -1,7 +1,7 @@
 node {
   stage('Archive Git Log') {
     withCredentials([usernameColonPassword(credentialsId: 'Test_Cred', variable: 'testID')]) {
-      git branch: 'main', credentialsID: 'Test_Cred', poll: false, url: 'git@github.com:eleone61/Cloned_repo.git'
+      git branch: 'main', credentialsId: 'Test_Cred', poll: false, url: 'git@github.com:eleone61/Cloned_repo.git'
       sh """
            ls -al
            git log "https://github.com/eleone61/jenkins-aws-upload.git" > git.log
