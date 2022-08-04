f21be715322220fd2f88829556bcf9164dc447f2
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-08-04 15:09:19 -0400

    Update achiveGitLog.groovy

--- achiveGitLog.groovy
+++ achiveGitLog.groovy
@@ -1,6 +1,6 @@
 node {
   stage('Archive Git Log') {
-    withCredentials([string(credentialsID: 'testID', variable: 'testID')]) {
+    withCredentials([[$class: 'StringBinding', credentialsID: 'testID', variable: 'testID']]) {
       
       git branch: 'main', credentialsID: testID, poll: false, url: 'git@github.com:eleone61/Cloned_repo.git'
       sh """
