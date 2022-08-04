bd4551676fbecdadf5b8849363bad0ac70f60b80
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-08-04 16:31:13 -0400

    Update achiveGitLog.groovy

--- achiveGitLog.groovy
+++ achiveGitLog.groovy
@@ -1,11 +1,12 @@
 node {
   stage('Archive Git Log') {
-    //withCredentials([sshUserPrivateKey(credentialsId: 'testCred', keyFileVariable: 'key')]) {
-       git branch: 'main', credentialsId: 'testCred', poll: false, url: 'git@github.com:eleone61/Cloned_repo.git'
+    withCredentials([sshUserPrivateKey(credentialsId: 'testCred', keyFileVariable: 'key')]) {
+       git branch: 'main', credentialsId: 'key', poll: false, url: 'git@github.com:eleone61/Cloned_repo.git'
       sh """
            ls -al
            git log > git.log
           """
+    }
     
 
       archiveArtifacts artifacts: "git.log", fingerprint: true
