7c1b754a969399d155b44f67884f8c5b3e2ebf27
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-10-25 13:46:11 -0400

    Update Jenkins(1).groovy

--- Jenkins(1).groovy
+++ Jenkins(1).groovy
@@ -8,8 +8,8 @@ node{
 
     while (pipelineENV != 'PREPROD') {
         pipelineENV = envSelect()
-        crCheck()
-        buildDescrpt()
+        crCheck(pipelineENV["CR"])
+        buildDescrpt(pipelineENV["CR"])
     }
 
     stage('Finished') {
@@ -55,8 +55,8 @@ def envSelect (){
 }
 
 
-def crCheck() {
-    def CR = pipelineENV.CR
+def crCheck(changeRequest) {
+    def CR = "${changeRequest}"
     println(CR)
     sh """
             if [[ ${CR} =~ N/A ]] || [[ ${CR} =~ n/a ]] || [[ ${CR} =~ N/a ]] || [[ ${CR} =~ n/A ]]; then 
