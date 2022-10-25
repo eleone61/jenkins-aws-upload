5106ffdeb5314cdbe4262c9d6493fc796a4ef51a
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-10-25 12:54:53 -0400

    Update Jenkins(1).groovy

--- Jenkins(1).groovy
+++ Jenkins(1).groovy
@@ -1,102 +1,83 @@
+def pipelineENV = ["env":"",
+                   "CR":""]
+
 node{
     stage('start') {
         echo 'start'
     }
 
-    def pipelineENV =''
     while (pipelineENV != 'PREPROD') {
         pipelineENV = envSelect()
+        crCheck(pipelineENV["CR"])
+        buildDescrpt(pipelineENV["CR"])
     }
-
-    approvalGate('PROD')
-    stagePROD()
 
     stage('Finished') {
         echo 'finished'
     }   
 }
 
-def approvalGate(env) {
-    stage("${env} approval gate"){
-        def req = input message: "Approve to deploy to ${env}", 
-            parameters: [string(description: 'Change request from KISAM', name: 'changeRequest')],
-            id: 'approver'
-        echo req
-    }
-
-}
-
 def envSelect (){
-    stage('environment select') {
-        def req = input message: 'Select an environment',
-        id: 'envResponse',
-        parameters: [choice(name: 'Branch to deploy',
-                    choices: "DEV\nDSIT\nPTE\nPREPROD\nPROD"
-                    )]
-
-        if (req != 'PROD') {
-            approvalGate(req)
-        }
-
-        switch(req) {
-            case 'DEV': 
-                stageDEV()
-                break;
-            case 'DSIT': 
-                stageDSIT()
-                break;
-            case 'PTE':
-                stagePTE()
-                break;
-            case 'PREPROD':
-                stagePREPROD()
-                break;
-            case 'PROD':
-                prodError()
-                break;
-            
-        }
-        return req
-}
-}
-
-def stageDEV (){
-    stage('deploying to DEV'){
-        echo 'DEV'
+	stage('environment select') {
+		envTestList = ["DSIT","SITE","PTE","EITE","END"]
+		
+		def req = ""
+		
+		timeout(time:1, unit:'DAYS') {
+			req = input message: "Select an environment to deploy Artifact?",
+				id: 'envResponse',
+			    	parameters: [string(description: 'Change Request number from KISAM', name: 'changeRequest'),
+					     choice(name: 'environment',
+						    choices: envTestList)]
+			println(req)
+		}
+		
+		if (req.environment == 'END') {
+			return req
+			break;
+		}
+		
+		if (req.environment != 'PROD') {
+			while ( req.changeRequest == "" ) {
+				req = input message: "Select an environment to deploy Artifact?",
+			   		id: 'DeployPackage',
+			   		parameters: [string(description: 'Missing KISAM change request number', name: 'changeRequest'),
+						     choice(name: 'environment',
+				      			    choices: envTestList)]
+				println(req)
+				if (req.environtment == 'END') {
+					return req
+					break;
+				}
+			}
+		}
     }
 }
 
-def stageDSIT (){
-    stage('deploying to DSIT'){
-        echo 'DSIT'
-    }
+
+def crCheck(changeRequest) {
+    CR = changeRequest
+    sh """
+            if [[ "${CR}" =~ N/A ]] || [[ "${CR}" =~ n/a ]] || [[ "${CR}" =~ N/a ]] || [[ "${CR}" =~ n/A ]]; then # $1 because it corresponds to the posistion of the parameter after the function name
+                echo "${CR} is valid"
+                break
+            else
+                if [ ${#1} -ge 5 ]; then
+                    if [[ "${CR}" =~ [0-9] ]] || [[ "${CR}" =~ [a..z] ]]; then
+                        echo "${CR} is valid!"
+                    else
+                        echo "${CR} is not valid"
+                    fi
+                else
+                    echo "${CR} is less than 5 characters"
+                fi
+            fi
+        """
 }
 
-def stagePTE (){
-    stage('deploying to PTE'){
-        echo 'PTE'
-    }
-}
-
-def stagePREPROD (){
-    stage('deploying to PREPROD') {
-        echo 'PREPROD'
-    }
-}
-
-def prodError() {
-    stage('Attempted to go to PROD, need to go to PREPROD first') {
-        catchError(stageResult: 'FAILURE') {
-            sh "exit 1"
-        }
-        echo 'must go to pre-prod first'
-        echo 'go to PREPROD first'
-    }
-}
-
-def stagePROD (){
-    stage('deploying to PROD') {
-        echo 'PROD'
-    }
+def buildDescript(changeRequest) {
+    CR = changeRequest.toString()
+    println(CR)
+    currentBuild.description = CR
 }
 
