3c802e4bf47ccc7ad0775ed15eabea2152f96b14
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-10-25 12:57:13 -0400

    Update Jenkins(1).groovy

--- Jenkins(1).groovy
+++ Jenkins(1).groovy
@@ -58,18 +58,18 @@ def envSelect (){
 def crCheck(changeRequest) {
     CR = changeRequest
     sh """
-            if [[ "${CR}" =~ N/A ]] || [[ "${CR}" =~ n/a ]] || [[ "${CR}" =~ N/a ]] || [[ "${CR}" =~ n/A ]]; then # $1 because it corresponds to the posistion of the parameter after the function name
-                echo "${CR} is valid"
+            if [[ "$CR" =~ N/A ]] || [[ "$CR" =~ n/a ]] || [[ "$CR" =~ N/a ]] || [[ "$CR" =~ n/A ]]; then # $1 because it corresponds to the posistion of the parameter after the function name
+                echo "$CR is valid"
                 break
             else
-                if [ ${#1} -ge 5 ]; then
-                    if [[ "${CR}" =~ [0-9] ]] || [[ "${CR}" =~ [a..z] ]]; then
-                        echo "${CR} is valid!"
+                if [ ${#CR} -ge 5 ]; then
+                    if [[ "$CR" =~ [0-9] ]] || [[ "${CR}" =~ [a..z] ]]; then
+                        echo "$CR is valid!"
                     else
-                        echo "${CR} is not valid"
+                        echo "$CR is not valid"
                     fi
                 else
-                    echo "${CR} is less than 5 characters"
+                    echo "$CR is less than 5 characters"
                 fi
             fi
         """
