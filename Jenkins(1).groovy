41880caadcd1a87c4c07232fb7386ecdc6ab6bb5
eleone61 <104224558+eleone61@users.noreply.github.com>
2022-10-28 10:46:57 -0400

    Update Jenkins(1).groovy

--- Jenkins(1).groovy
+++ Jenkins(1).groovy
@@ -1,6 +1,6 @@
 def pipelineENV = ["env":"",
                    "changeRequest":""]
-crValid = true
+// crValid = true
 
 node{
     stage('start') {
@@ -67,7 +67,7 @@ def crCheck(changeRequest) {
     	if [[ ${CR} =~ N/A ]] || [[ ${CR} =~ n/a ]] || [[ ${CR} =~ N/a ]] || [[ ${CR} =~ n/A ]]
 	then
 		echo "${CR} is valid"
-		$crValid = 1
+		crValid = 1
     		break
 	else
 		if [ ${crLen} -ge 5 ]
@@ -77,11 +77,11 @@ def crCheck(changeRequest) {
 			    echo "${CR} is valid!"
 			else
 			    echo "${CR} is not valid"
-			    echo $crValid = 0
+			    crValid = 0
 			fi
 		else
 			echo "${CR} is less than 5 characters"
-			$crValid = 0
+			crValid = 0
 		fi
 	fi
 	"""
